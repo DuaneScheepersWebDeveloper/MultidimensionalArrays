7516cb2bc7d86933791875909a8c4120d17134a5
Duane Scheepers <98160374+DuaneScheepersWebDeveloper@users.noreply.github.com>
2023-08-31 09:46:53 +0200

    Initial commit

---
+++ MultidimensionalArrays/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace MultidimensionalArrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Declare and initialize a 2D Array
+            int[,] array2D = new int[,]
+            {
+                {1, 2, 3}, // Row 0
+                {4, 5, 6}, // Row 1
+                {7, 8, 9}  // Row 2
+            };
+
+            // Access and print a value from the array
+            Console.WriteLine("Value at array2D[2, 0] is {0}", array2D[0, 1]);
+
+            // Wait for user input before exiting
+            Console.ReadKey();
+        }
+    }
+}
